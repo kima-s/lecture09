ab7db32f6414287adb0f99338599b6dad31b5cc0
kima-s <kimasyun@gmail.com>
2023-07-14 13:40:09 +0900

    レスポンスの検証テストを追加、存在しないIDで更新・削除した際のテストを追加

--- src/test/java/com/raisetech/mybatisdemo/integrationtest/UserRestApiIntegrationTest.java
+++ src/test/java/com/raisetech/mybatisdemo/integrationtest/UserRestApiIntegrationTest.java
@@ -5,8 +5,10 @@ import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import com.github.database.rider.spring.api.DBRider;
 import com.raisetech.mybatisdemo.exception.ResourceNotFoundException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.Customization;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.skyscreamer.jsonassert.comparator.CustomComparator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -73,7 +75,7 @@ public class UserRestApiIntegrationTest {
     @Test
     @DataSet(value = "datasets/users.yml")
     @Transactional
-    void 存在しないユーザーのIDを指定したときに例外が返されること() throws Exception {
+    void 存在しないIDを指定してユーザーを取得するときに例外が返されること() throws Exception {
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/names/99"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound())
                 .andReturn();
@@ -84,10 +86,30 @@ public class UserRestApiIntegrationTest {
 
     @Test
     @DataSet(value = "datasets/users.yml")
+    @Transactional
+    void 存在しないIDを指定してユーザーを取得するときにNotFoundレスポンスが返されること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.get("/names/99"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                   "message": "resource not found",
+                   "timestamp": "2023-07-11T23:00:01.451993+09:00[Asia/Tokyo]",
+                   "error": "Not Found",
+                   "path": "/names/99",
+                   "status": "404"
+                }
+                """, response, new CustomComparator(JSONCompareMode.STRICT,
+                new Customization("timestamp", ((o1, o2) -> true))));
+    }
+
+    @Test
+    @DataSet(value = "datasets/users.yml")
     @ExpectedDataSet(value = "datasets/expectedUsers.yml", ignoreCols = "id")
     @Transactional
     void ユーザーを１件登録できること() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/users")
+        String response = mockMvc.perform(MockMvcRequestBuilders.post("/users")
                         .contentType(MediaType.APPLICATION_JSON).content("""
                                 {
                                    "id": 4,
@@ -96,7 +118,14 @@ public class UserRestApiIntegrationTest {
                                    "age": 38
                                 }
                                 """))
-                .andExpect(MockMvcResultMatchers.status().isCreated());
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                   "message": "user successfully created!"
+                }
+                """, response, JSONCompareMode.STRICT);
     }
 
     @Test
@@ -104,7 +133,7 @@ public class UserRestApiIntegrationTest {
     @ExpectedDataSet(value = "datasets/expectedUsers2.yml")
     @Transactional
     void ユーザーを１件更新できること() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.patch("/users/3")
+        String response = mockMvc.perform(MockMvcRequestBuilders.patch("/users/3")
                         .contentType(MediaType.APPLICATION_JSON).content("""
                                 {
                                    "id": 3,
@@ -113,7 +142,42 @@ public class UserRestApiIntegrationTest {
                                    "age": 38
                                 }
                                 """))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                   "message": "user successfully updated"
+                }
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/users.yml")
+    @Transactional
+    void 存在しないIDを指定してユーザーを更新するときにNotFoundレスポンスが返されること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.patch("/users/99")
+                        .contentType(MediaType.APPLICATION_JSON).content("""
+                                {
+                                   "id": 99,
+                                   "name": "山中",
+                                   "address": "Mie",
+                                   "age": 38
+                                }
+                                """))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                   "message": "resource not found",
+                   "timestamp": "2023-07-11T23:00:01.451993+09:00[Asia/Tokyo]",
+                   "error": "Not Found",
+                   "path": "/users/99",
+                   "status": "404"
+                }
+                """, response, new CustomComparator(JSONCompareMode.STRICT,
+                new Customization("timestamp", ((o1, o2) -> true))));
     }
 
     @Test
@@ -121,7 +185,34 @@ public class UserRestApiIntegrationTest {
     @ExpectedDataSet(value = "datasets/expectedUsers3.yml")
     @Transactional
     void ユーザーを１件削除できること() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.delete("/users/3"))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+        String response = mockMvc.perform(MockMvcRequestBuilders.delete("/users/3"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+
+        JSONAssert.assertEquals("""
+                {
+                   "message": "user successfully deleted"
+                }
+                """, response, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DataSet(value = "datasets/users.yml")
+    @Transactional
+    void 存在しないIDを指定してユーザーを削除するときにNotFoundレスポンスが返されること() throws Exception {
+        String response = mockMvc.perform(MockMvcRequestBuilders.delete("/users/99"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound())
+                .andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+        
+        JSONAssert.assertEquals("""
+                {
+                   "message": "resource not found",
+                   "timestamp": "2023-07-11T23:00:01.451993+09:00[Asia/Tokyo]",
+                   "error": "Not Found",
+                   "path": "/users/99",
+                   "status": "404"
+                }
+                """, response, new CustomComparator(JSONCompareMode.STRICT,
+                new Customization("timestamp", ((o1, o2) -> true))));
     }
 }
